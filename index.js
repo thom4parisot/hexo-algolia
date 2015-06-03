6d0ae2f7c8130b17d5e228b0af71cbbf11c5daf2
Boris Schapira <bschapira@clever-age.com>
2015-06-03 09:41:51 +0200

    Through the console + filter angle

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ var config = hexo.config.algolia = assign({
 	chunkSize: 5000
 }, hexo.config.algolia);
 
-hexo.extend.generator.register('algolia-index', require('./lib/generator'));
+// hexo.extend.generator.register('algolia-index', require('./lib/generator'));
+hexo.extend.console.register('algolia', 'Index your content in Algolia Search API', {
+  options: [
+    {name: '-f, --fake', desc: 'Does not push content to Algolia'}
+  ]
+}, require('./lib/command'));
