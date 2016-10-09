8e8028fde5b1aa22f9ef0c185e8c7ca4cf12c969
oncletom <hi@oncletom.io>
2016-10-09 23:09:57 +0100

    Use hexo.load() and query the db instead of relying on hooks and events

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var config = hexo.config.algolia = assign({
 	chunkSize: 5000
 }, hexo.config.algolia);
 
-var box = hexo.source;
-
 hexo.extend.console.register('algolia', 'Index your content in Algolia Search API', {
   options: [
     {name: '-f, --fake', desc: 'Does not push content to Algolia'},
