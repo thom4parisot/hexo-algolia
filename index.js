546b30067019dd10052e9b8c71e59d41a0ba9262
Thomas Parisot <hi@oncletom.io>
2017-06-07 11:06:18 +0200

    chore(*): Remove unused call for lodash.assign

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /* globals hexo:false, console:false*/
 'use strict';
 
-var assign = require('lodash').assign;
-
 var command = require('./lib/command');
 var searchCdnHelper = require('./lib/helpers/search_cdn.js');
 var searchConfigHelper = require('./lib/helpers/search_config.js');
